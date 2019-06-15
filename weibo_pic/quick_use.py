df495892a8dbca1cc62dd5f890c80b65bb5adfa3
Void-King <1402112011@qq.com>
2019-06-15 14:28:09 +0800

    Void

--- weibo_pic/quick_use.py
+++ weibo_pic/quick_use.py
@@ -1,7 +1,26 @@
 import os
 import sys
-name = input('输入微博昵称：\n')
-cmda = 'cmd /k cd ' + sys.path[0] + '& python weibo_download.py -u ' + name
-r = os.system(cmda)
-print (r.read())
-r.close()
+import tkinter as tk
+
+root = tk.Tk()
+root.title('weibo pic')
+frame = tk.Frame(root)
+frame.pack(padx = 20, pady = 30)
+
+label = tk.Label(frame, text = '输入微博昵称：')
+label.grid(row = 0, column = 0, sticky = tk.W)
+
+ipt = tk.Entry(frame, width = 30)
+ipt.grid(row = 1, column = 0)
+
+def download():
+    name = ipt.get()
+    cmd = 'cd ' + sys.path[0] + '& start cmd /k python weibo_download.py -u ' + name
+    os.system(cmd)
+
+conf = tk.Button(frame, text = '下载', command = download)
+conf.grid(row = 1, column = 1, padx = 10)
+root.mainloop()
+# name = input('输入微博昵称：\n')
+# cmd = 'cd ' + sys.path[0] + '& start cmd /k python weibo_download.py -u ' + name
+# os.system(cmd)
